b88d41e0d0e6b769761be4e6c429be898e58919d
web development <vyshnavycm@gmail.com>
2026-01-04 18:23:11 +0530

    navigation bar is activated

--- physics_mentor/website/urls.py
+++ physics_mentor/website/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+# from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path("about/", views.about, name="about"),
+    path("courses/", views.courses, name="courses"),
+    path("features/", views.features, name="features"),
+    path("testimonials/", views.testimonials, name="testimonials"),
+    path("contact/", views.contact, name="contact"),
 ]
--- physics_mentor/website/views.py
+++ physics_mentor/website/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'website/home.html')
+
+def about(request):
+    return render(request, "website/about.html")
+
+def courses(request):
+    return render(request, "website/courses.html")
+
+def features(request):
+    return render(request, "website/features.html")
+
+def testimonials(request):
+    return render(request, "website/testimonials.html")
+
+def contact(request):
+    return render(request, "website/contact.html")
